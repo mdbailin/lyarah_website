f92c80e3e5252bf516f13686ceb8ca2ec79c2476
Matthew Bailin <matthewdbailin@gmail.com>
2023-05-10 22:42:38 +0700

    removed /

--- constants/index.js
+++ constants/index.js
@@ -34,7 +34,7 @@ export const startingFeatures = [
 
 export const newFeatures = [
   {
-    imgUrl: './vrpano.svg',
+    imgUrl: 'vrpano.svg',
     title: 'A dedicated mission',
     subtitle:
         'L\'yarah means "to teaching" (sort of like l\'chaim!) and is at the core of our social mission. We believe that the best math teachers have the power to inspire, transform, and revitalize.',
@@ -49,19 +49,19 @@ export const newFeatures = [
 
 export const insights = [
   {
-    imgUrl: '/planet-06.png',
+    imgUrl: 'planet-06.png',
     title: 'The launch of the Metaverse makes Elon musk ketar-ketir',
     subtitle:
         'Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Diam maecenas sed enim ut sem viverra alique.',
   },
   {
-    imgUrl: './planet-07.png',
+    imgUrl: 'planet-07.png',
     title: '7 tips to easily master the madness of the Metaverse',
     subtitle:
         'Vitae congue eu consequat ac felis donec. Et magnis dis parturient montes nascetur ridiculus mus. Convallis tellus id interdum',
   },
   {
-    imgUrl: './planet-08.png',
+    imgUrl: 'planet-08.png',
     title: 'With one platform you can explore the whole world virtually',
     subtitle:
         'Quam quisque id diam vel quam elementum. Viverra nam libero justo laoreet sit amet cursus sit. Mauris in aliquam sem',
@@ -71,18 +71,18 @@ export const insights = [
 export const socials = [
   {
     name: 'twitter',
-    url: '/twitter.svg',
+    url: 'twitter.svg',
   },
   {
     name: 'linkedin',
-    url: '/linkedin.svg',
+    url: 'linkedin.svg',
   },
   {
     name: 'instagram',
-    url: '/instagram.svg',
+    url: 'instagram.svg',
   },
   {
     name: 'facebook',
-    url: '/facebook.svg',
+    url: 'facebook.svg',
   },
 ];
